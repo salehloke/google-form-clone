c6bcd6d50a5266ec0f771ba272490f9fa5efa12a
salehloke <salehloke@gmail.com>
2022-02-14 23:44:42 +0800

    2 files added

---
+++ src/app/google-form-clone/google-form-clone-model/google-form-clone-model.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-form-clone-model',
+  templateUrl: './google-form-clone-model.component.html',
+  styleUrls: ['./google-form-clone-model.component.css'],
+})
+export class GoogleFormCloneModelComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

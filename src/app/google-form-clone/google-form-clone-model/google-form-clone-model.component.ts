674418f25d42b2d7b36dd5a309cdcd16b9e7d3c6
salehloke <salehloke@gmail.com>
2022-02-14 23:58:16 +0800

    3 files modified, 1 added

--- src/app/google-form-clone/google-form-clone-model/google-form-clone-model.component.ts
+++ src/app/google-form-clone/google-form-clone-model/google-form-clone-model.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-google-form-clone-model',
+  selector: 'google-form-clone-model',
   templateUrl: './google-form-clone-model.component.html',
   styleUrls: ['./google-form-clone-model.component.css'],
 })
 export class GoogleFormCloneModelComponent implements OnInit {
+  @Input() parentQuestions;
+
   constructor() {}
 
   ngOnInit() {}
